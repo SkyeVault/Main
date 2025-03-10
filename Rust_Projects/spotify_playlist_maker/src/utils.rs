36b25f663bfbd4f1f83dcd0828102d20f3c4ddcb
Lorelei Noble <64027767+SkyeVault@users.noreply.github.com>
2025-03-09 20:10:50 -0400

    Removed utils.rs and updated project structure

--- Rust_Projects/spotify_playlist_maker/src/utils.rs
+++
@@ -1,9 +0,0 @@
-#[allow(dead_code)] // This prevents warnings about unused functions
-pub fn format_track_name(title: &str, artist: &str) -> String {
-    format!("{} - {}", title, artist)
-}
-
-#[allow(dead_code)]
-pub fn generate_spotify_auth_url() -> String {
-    "https://accounts.spotify.com/authorize".to_string()
-}
