d101a8c75fc2ac518305aa9d62edf8b94e4693f7
Lorelei Noble | Full-Stack DevSecOps Engineer <64027767+SkyeVault@users.noreply.github.com>
2025-03-11 19:50:50 -0400

    Update MetaverseLand.sol

--- Blockchain_Dev/metaverse-sandbox/blockchain/contracts/MetaverseLand.sol
+++ Blockchain_Dev/metaverse-sandbox/blockchain/contracts/MetaverseLand.sol
@@ -1,1 +1,21 @@
-// ERC-721 Land ownership contract
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// ERC-721 NFT contract for virtual land ownership in the metaverse.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MetaverseLand is ERC721, Ownable {
+    uint256 public nextLandId;
+    
+    constructor() ERC721("MetaverseLand", "MLAND") {}
+
+    function mintLand(address to) public onlyOwner {
+        _mint(to, nextLandId);
+        nextLandId++;
+    }
+}
+
+// How to Use:
+// 1. Deploy the contract with Hardhat or Foundry.
+// 2. Call `mintLand(userAddress)` to assign land NFTs.
