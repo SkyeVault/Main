1567f3e360e9fa0e5b088a9334c485b59a6f1289
Lorelei Noble <64027767+SkyeVault@users.noreply.github.com>
2025-03-13 20:15:47 -0400

    updates

--- Blockchain/metaverse-sandbox/blockchain/src/MetaverseLand.sol
+++ Blockchain/metaverse-sandbox/blockchain/src/MetaverseLand.sol
@@ -1,18 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
-// ERC-721 NFT contract for virtual land ownership in the metaverse.
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MetaverseLand is ERC721, Ownable {
-    uint256 public nextLandId;
-    
-    constructor() ERC721("MetaverseLand", "MLAND") {}
+    constructor() ERC721("MetaverseLand", "MTL") Ownable(msg.sender) {}
 
-    function mintLand(address to) public onlyOwner {
-        _mint(to, nextLandId);
-        nextLandId++;
+    // Example function (replace with your own logic)
+    function mintLand(address to, uint256 tokenId) external onlyOwner {
+        _safeMint(to, tokenId);
     }
 }
 
