13e6d7ee72cf5810a80b2819aeffe55b41a145ee
Lorelei Noble | Full-Stack Developer <64027767+SkyeVault@users.noreply.github.com>
2025-04-01 16:12:46 -0400

    Update config.ts

--- rainkeep/src/content/config.ts
+++ rainkeep/src/content/config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from 'astro:content';
 const blog = defineCollection({
   schema: z.object({
     title: z.string(),
-    pubDate: z.date(),
+    pubDate: z.coerce.date(),
     description: z.string(),
   }),
 });
