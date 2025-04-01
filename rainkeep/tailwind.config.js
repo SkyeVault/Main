84e46a58dcbeadae63d288066bb5d8619261e9c2
Lorelei Noble | Full-Stack DevSecOps Engineer <64027767+SkyeVault@users.noreply.github.com>
2025-04-01 12:34:11 -0400

    Update tailwind.config.js

--- rainkeep/tailwind.config.js
+++ rainkeep/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}',
+    './src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}', // Add other file types if needed
   ],
   theme: {
-    extend: {},
+    extend: {}, // Optional custom stuff here
   },
   plugins: [],
 };
