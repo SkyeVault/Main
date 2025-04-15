de29f067701c437166df8ebc17152581fc2cc430
Lorelei Noble <64027767+SkyeVault@users.noreply.github.com>
2025-04-15 13:38:11 -0400

    edit 3d gallery

--- rainkeep/public/gallery3d/vite.config.js
+++ rainkeep/public/gallery3d/vite.config.js
@@ -1,9 +1,10 @@
+// ~/main/rainkeep/public/gallery3d/vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/gallery3d/',
   build: {
-    outDir: '../../../dist/gallery3d',
+    outDir: '../../../rainkeep/dist/gallery3d',
     emptyOutDir: false
   }
 });
