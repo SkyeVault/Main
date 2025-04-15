5231febf3c904d95c788b4a40ae2833413b0fba5
Lorelei Noble | Full-Stack Developer <64027767+SkyeVault@users.noreply.github.com>
2025-04-15 17:32:44 -0400

    Update main.js

--- rainkeep/public/gallery3d/main.js
+++ rainkeep/public/gallery3d/main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://cdn.skypack.dev/three';
-import { OrbitControls } from 'https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://esm.sh/three@0.158.0';
+import { OrbitControls } from 'https://esm.sh/three@0.158.0/examples/jsm/controls/OrbitControls.js';
 
 const canvas = document.querySelector('#scene');
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
